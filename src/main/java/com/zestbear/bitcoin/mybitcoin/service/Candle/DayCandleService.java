730fe0c4a17e2c3a5e450d84787609a3d4532f50
HYUN <zestbear90@gmail.com>
2023-08-27 17:23:59 +0900

    Day #6 비동기 처리를 위한 CompletableFuture

--- src/main/java/com/zestbear/bitcoin/mybitcoin/service/Candle/DayCandleService.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/service/Candle/DayCandleService.java
@@ -1,7 +1,7 @@
 package com.zestbear.bitcoin.mybitcoin.service.Candle;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zestbear.bitcoin.mybitcoin.domain.Candle.DayCandle;
+import com.zestbear.bitcoin.mybitcoin.domain.Candle.DayCandleData;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -14,37 +14,41 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class DayCandleService {
 
-    public Map<String, List<DayCandle>> getDayCandleData() {
-        String[] coinSymbols = {"BTC", "ETH", "XRP", "ADA", "DOGE", "SOL", "TRX", "DOT", "MATIC", "BCH"};
-        String urlTemplate = "https://api.upbit.com/v1/candles/days?market=KRW-%s&count=120&convertingPriceUnit=KRW";
-        Map<String, List<DayCandle>> resultMap = new HashMap<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+    public CompletableFuture<Map<String, List<DayCandleData>>> getDayCandleData() {
+        return CompletableFuture.supplyAsync(() -> {
+            String[] coinSymbols = {"BTC", "ETH", "ADA", "DOT", "MATIC"};
+            String urlTemplate = "https://api.upbit.com/v1/candles/days?market=KRW-%s&count=21&convertingPriceUnit=KRW";
+            Map<String, List<DayCandleData>> resultMap = new HashMap<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        for (String coinSymbol : coinSymbols) {
-            String url = String.format(urlTemplate, coinSymbol);
-            String result = "";
-            HttpGet httpGet = new HttpGet(url);
+            for (String coinSymbol : coinSymbols) {
+                String url = String.format(urlTemplate, coinSymbol);
+                String result = "";
+                HttpGet httpGet = new HttpGet(url);
 
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    result = EntityUtils.toString(entity);
+                try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null) {
+                        result = EntityUtils.toString(entity);
+                    }
+
+                    DayCandleData[] dayCandleData = objectMapper.readValue(result, DayCandleData[].class);
+                    resultMap.put(coinSymbol, Arrays.asList(dayCandleData));
+//                  for (DayCandleData data : dayCandleData) {
+//                      System.out.println(objectMapper.writeValueAsString(data));
+//                  }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
-                DayCandle[] dayCandleData = objectMapper.readValue(result, DayCandle[].class);
-                resultMap.put(coinSymbol, Arrays.asList(dayCandleData));
-//                for (DayCandle data : dayCandleData) {
-//                    System.out.println(objectMapper.writeValueAsString(data));
-//                }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
-        return resultMap;
+
+            return resultMap;
+        });
     }
 }
--- src/main/java/com/zestbear/bitcoin/mybitcoin/service/Candle/DayMovingAverageLineService.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/service/Candle/DayMovingAverageLineService.java
@@ -1,10 +1,12 @@
 package com.zestbear.bitcoin.mybitcoin.service.Candle;
 
-import com.zestbear.bitcoin.mybitcoin.domain.Candle.DayCandle;
+import com.zestbear.bitcoin.mybitcoin.domain.Candle.DayCandleData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -12,37 +14,46 @@ public class DayMovingAverageLineService {
 
     private final DayCandleService dayCandleService;
 
-    public Map<String, Map<Integer, List<Long>>> getMovingAverageLine() {
-        Map<String, List<DayCandle>> jsonMap = dayCandleService.getDayCandleData();
-        List<Integer> periods = Arrays.asList(5, 10, 20, 60, 120);
+    public Map<String, Map<Integer, Double>> getMovingAverageLine() {
+        CompletableFuture<Map<String, List<DayCandleData>>> future = dayCandleService.getDayCandleData();
+        Map<String, List<DayCandleData>> jsonMap = null;
+        try {
+            jsonMap = future.get();  // 비동기 작업이 완료될 때까지 대기
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return Collections.emptyMap();  // 오류가 발생하면 빈 맵을 반환
+        }
 
-        Map<String, Map<Integer, List<Long>>> movingAveragesMap = new HashMap<>();
+        List<Integer> periods = Arrays.asList(5, 10, 21);
+
+        Map<String, Map<Integer, Double>> movingAveragesMap = new HashMap<>();
 
         for (String coinSymbol : jsonMap.keySet()) {
             try {
-                List<DayCandle> sortedDayCandles = new ArrayList<>(jsonMap.get(coinSymbol));
-                sortedDayCandles.sort(Comparator.comparing(DayCandle::getCandle_date_time_kst));
+                List<DayCandleData> sortedDayCandleData = new ArrayList<>(jsonMap.get(coinSymbol));
+                sortedDayCandleData.sort(Comparator.comparing(DayCandleData::getCandle_date_time_kst));
 
-                Map<Integer, List<Long>> coinMovingAverages = new HashMap<>();
+                Map<Integer, Double> coinMovingAverages = new HashMap<>();
 
                 for (int period : periods) {
-                    if (sortedDayCandles.size() >= period) { // 데이터가 충분한 경우만 처리
-                        double sum = sortedDayCandles.subList(sortedDayCandles.size() - period,
-                                        sortedDayCandles.size()).stream()
-                                .mapToDouble(DayCandle::getTrade_price)
+                    if (sortedDayCandleData.size() >= period) { // 데이터가 충분한 경우만 처리
+                        double sum = sortedDayCandleData.subList(sortedDayCandleData.size() - period,
+                                        sortedDayCandleData.size()).stream()
+                                .mapToDouble(DayCandleData::getTrade_price)
                                 .sum();
 
-                        // 해당 기간의 이동평균값 리스트를 맵에 추가
-                        coinMovingAverages.put(period, Collections.singletonList((long) sum / period));
+                        System.out.println(coinSymbol + " (" + period + "-day): " + sum / period);
+                        // 해당 기간의 이동평균값을 맵에 추가
+                        coinMovingAverages.put(period,sum / period);
                     }
                 }
-
                 movingAveragesMap.put(coinSymbol, coinMovingAverages);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         return movingAveragesMap;
     }
 }
