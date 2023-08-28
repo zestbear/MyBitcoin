162f9977aa95a0f0c33b94777142b03e7665f6b4
HYUN <zestbear90@gmail.com>
2023-08-28 23:25:59 +0900

    Day #7 @EnableCaching 추가

--- src/main/java/com/zestbear/bitcoin/mybitcoin/MybitcoinApplication.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/MybitcoinApplication.java
@@ -2,10 +2,12 @@ package com.zestbear.bitcoin.mybitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableAsync
 @EnableScheduling
 @EnableJpaAuditing
