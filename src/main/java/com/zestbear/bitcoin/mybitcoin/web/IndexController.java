90401011a1b403ab43a0ac712ce41005593fdf02
HYUN <zestbear90@gmail.com>
2023-08-29 17:49:48 +0900

    Day #8 실행을 위한 Controller

--- src/main/java/com/zestbear/bitcoin/mybitcoin/web/IndexController.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/web/IndexController.java
@@ -1,27 +1,39 @@
 package com.zestbear.bitcoin.mybitcoin.web;
 
-import com.zestbear.bitcoin.mybitcoin.domain.Candle.CandleData;
-import com.zestbear.bitcoin.mybitcoin.service.CandleService;
+import com.zestbear.bitcoin.mybitcoin.service.Account.AccountService;
+import com.zestbear.bitcoin.mybitcoin.service.Account.CurrentAsset;
+import com.zestbear.bitcoin.mybitcoin.service.Candle.CurrentValueService;
+import com.zestbear.bitcoin.mybitcoin.service.Candle.DayMovingAverageLineService;
+import com.zestbear.bitcoin.mybitcoin.service.Order.Execute;
+import com.zestbear.bitcoin.mybitcoin.service.Order.OrderService;
+import com.zestbear.bitcoin.mybitcoin.service.Strategy.MovingAverageLineService;
+import com.zestbear.bitcoin.mybitcoin.service.Strategy.ProportionService;
 import lombok.RequiredArgsConstructor;
+import okhttp3.OkHttpClient;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
-    private final CandleService candleService;
+    private final Execute execute;
 
-    @GetMapping("/main")
-    public String Index(Model model) {
-        String[] coinSymbols = {"BTC", "ETH", "ETC", "ADA"};
-        Map<String, List<CandleData>> jsonMap = candleService.getCandleData(coinSymbols);
+    @GetMapping("/")
+    public String Index() throws IOException, NoSuchAlgorithmException {
 
-        model.addAttribute("jsonMap", jsonMap);
+        execute.execute();
+
         return "index";
     }
 }
