d4a1639226695d7697a12897a36d3d01d6a30dfa
HYUN <zestbear90@gmail.com>
2023-08-29 17:49:31 +0900

    Day #8 API 매도 volume 로직 작성

--- src/main/java/com/zestbear/bitcoin/mybitcoin/service/Order/OrderService.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/service/Order/OrderService.java
@@ -4,7 +4,11 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.google.gson.Gson;
 import com.zestbear.bitcoin.mybitcoin.config.DecryptionUtils;
+import com.zestbear.bitcoin.mybitcoin.service.Account.CurrentAsset;
 import com.zestbear.bitcoin.mybitcoin.service.Account.UpbitAPIConfig;
+import com.zestbear.bitcoin.mybitcoin.service.Candle.CurrentValueService;
+import com.zestbear.bitcoin.mybitcoin.service.Strategy.PermissionService;
+import jakarta.annotation.PostConstruct;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -28,13 +31,25 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
-    private static final String SERVER_URL = "https://api.upbit.com";
+    private final PermissionService permissionService;
+    private final CurrentAsset currentAsset;
+    private final CurrentValueService currentValueService;
 
     private static String ACCESS_KEY;
     private static String SECRET_KEY;
 
     @Autowired
-    public OrderService(UpbitAPIConfig upbitAPIConfig) throws IOException {
+    private UpbitAPIConfig upbitAPIConfig;
+
+    @Autowired
+    public OrderService(PermissionService permissionService, CurrentAsset currentAsset, CurrentValueService currentValueService) {
+        this.permissionService = permissionService;
+        this.currentAsset = currentAsset;
+        this.currentValueService = currentValueService;
+    }
+
+    @PostConstruct
+    public void init() {
         try {
             ACCESS_KEY = DecryptionUtils.decrypt(upbitAPIConfig.getACCESS_KEY(), upbitAPIConfig.getKEY());
             SECRET_KEY = DecryptionUtils.decrypt(upbitAPIConfig.getSECRET_KEY(), upbitAPIConfig.getKEY());
@@ -43,52 +58,93 @@ public class OrderService {
         }
     }
 
-    public void postOrder(String[] coinSymbols) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public void postOrder(String coinSymbol) throws NoSuchAlgorithmException, IOException {
+
+        Map<String, Double> values = currentAsset.getValueforEach();
+        double current = currentAsset.getCurrentKRWAsset();
+        Map<String, Double> currentValues = currentValueService.getCurrent();
+
+        if (currentValues == null) {
+            System.out.println("Failed to retrieve current values");
+            return; // Exit the method early
+        }
+
+
+        String action = null;
+        try {
+            action = permissionService.isSafe(coinSymbol);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return; // if there's an exception, stop here.
+        }
+
+        if (action.equals("stay")) {
+            return; // Do nothing if we should stay
+        }
+
         HashMap<String, String> params = new HashMap<>();
-        params.put("market", "KRW-BTC");
-        params.put("side", "bid");          // 매수 : bid, 매도 : ask
-        params.put("volume", "1");
-        params.put("price", "5000");
-        params.put("ord_type", "limit");
+        params.put("market", "KRW-" + coinSymbol);
+
+        if(action.equals("bid")){
+            if (!values.containsKey(coinSymbol)) {
+                params.put("side", "bid");
+                params.put("ord_type", "price");
+                switch (coinSymbol) {
+                    case "BTC", "ETH" -> params.put("price", String.valueOf(current * 0.4));
+                    case "ADA" -> params.put("price", String.valueOf(current * 0.1));
+                    case "DOT", "MATIC" -> params.put("price", String.valueOf(current * 0.05));
+                }
+                System.out.println("BID: " + params.get("market") + " " + params.get("price"));
+            }
+        } else if (action.equals("ask")) {
+            if (values.containsKey(coinSymbol)) {
+                double volume = values.get(coinSymbol) / currentValues.get("KRW-" + coinSymbol);
+                params.put("side", "ask");
+                params.put("volume", String.format("%.8f", volume));
+                params.put("ord_type", "market");
+                System.out.println("ASK: " + params.get("market") + " " + params.get("volume"));
+                System.out.println("In my wallet: " + values.get(coinSymbol));
+            }
+        }
+
+        System.out.println("DEFAULT: " + params.get("market"));
 
         ArrayList<String> queryElements = new ArrayList<>();
-        for(
-                Map.Entry<String, String> entity : params.entrySet()) {
+        for(Map.Entry<String, String> entity : params.entrySet()) {
             queryElements.add(entity.getKey() + "=" + entity.getValue());
         }
 
         String queryString = String.join("&", queryElements.toArray(new String[0]));
 
         MessageDigest md = MessageDigest.getInstance("SHA-512");
-        md.update(queryString.getBytes("UTF-8"));
+        md.update(queryString.getBytes());
 
         String queryHash = String.format("%0128x", new BigInteger(1, md.digest()));
 
-        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-        String jwtToken = JWT.create()
+        Algorithm algorithmHMAC256= Algorithm.HMAC256(SECRET_KEY);
+        String jwtToken= JWT.create()
                 .withClaim("access_key", ACCESS_KEY)
                 .withClaim("nonce", UUID.randomUUID().toString())
-                .withClaim("query_hash", queryHash)
-                .withClaim("query_hash_alg", "SHA512")
-                .sign(algorithm);
+                .withClaim ("query_hash" , queryHash)
+                .withClaim ("query_hash_alg" , "SHA512")
+                .sign(algorithmHMAC256);
 
-        String authenticationToken = "Bearer " + jwtToken;
+        String authenticationToken= "Bearer "+jwtToken;
 
-        try {
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpPost request = new HttpPost(SERVER_URL + "/v1/orders");
-            request.setHeader("Content-Type", "application/json");
-            request.addHeader("Authorization", authenticationToken);
+        try{
+            HttpClient client= HttpClientBuilder.create().build();
+            String serverUrl = "https://api.upbit.com";
+            HttpPost request=new HttpPost(serverUrl +"/v1/orders");
+            request.setHeader ("Content-Type" , "application/json" );
+            request.addHeader ("Authorization" , authenticationToken);
             request.setEntity(new StringEntity(new Gson().toJson(params)));
 
-            HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
+            HttpResponse response=client.execute(request);
+            HttpEntity entity=response.getEntity();
 
             System.out.println(EntityUtils.toString(entity, "UTF-8"));
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
