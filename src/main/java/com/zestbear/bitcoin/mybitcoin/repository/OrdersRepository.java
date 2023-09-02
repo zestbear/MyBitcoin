bc91ef1ebadc795638707d1cb2c85c4c497ac758
HYUN <zestbear90@gmail.com>
2023-09-02 20:15:13 +0900

    Day #12 사용하지 않는 기능 삭제

--- src/main/java/com/zestbear/bitcoin/mybitcoin/repository/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zestbear.bitcoin.mybitcoin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
-}
-
