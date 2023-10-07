3570a9b567093009e9c23e76a923a4e302d4abdd
zestbear <zestbear90@gmail.com>
2023-10-08 00:29:14 +0900

    Day #16 Caching 삭제

--- src/main/java/com/zestbear/bitcoin/mybitcoin/MybitcoinApplication.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/MybitcoinApplication.java
@@ -2,11 +2,9 @@ package com.zestbear.bitcoin.mybitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
