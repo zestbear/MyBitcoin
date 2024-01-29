c8bf1f094c2afefc585ff03625b79573891ba960
zestbear <zestbear90@gmail.com>
2024-01-29 12:56:59 +0900

    :recycle: refactor: 동기화 로직, 로깅 수정

--- src/main/java/com/zestbear/bitcoin/mybitcoin/service/Execute/ExecuteService.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/service/Execute/ExecuteService.java
@@ -5,8 +5,7 @@ import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Account.CurrentAsset;
 import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Order.CancelOrderAPI;
 import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Order.OrderListAPI;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -16,35 +15,43 @@ import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-@RequiredArgsConstructor
+@Slf4j
 @Service
 public class ExecuteService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ExecuteService.class);
 
     private final APIService apiService;
     private final OrderService orderService;
     private final CancelOrderAPI cancelOrderAPI;
     private final OrderListAPI orderListAPI;
-
     private final CurrentAsset currentAsset;
 
-    @Scheduled(fixedDelay = 30000)
-    public void execute() throws IOException, NoSuchAlgorithmException, ExecutionException, InterruptedException {
+    public ExecuteService(APIService apiService, OrderService orderService, CancelOrderAPI cancelOrderAPI, OrderListAPI orderListAPI, CurrentAsset currentAsset) {
+        this.apiService = apiService;
+        this.orderService = orderService;
+        this.cancelOrderAPI = cancelOrderAPI;
+        this.orderListAPI = orderListAPI;
+        this.currentAsset = currentAsset;
+    }
 
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-            try {
-                orderListAPI.getOrders();
-            } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
-                logger.error("Error updating order list data", e);
-                throw new RuntimeException(e);
-            }
-        });
-        future.join();
+    /*
+        1. 미체결 주문들이 있다면 모두 불러오기
+        2. 미체결 주문들 모두 주문 취소
+        3. 모든 API로 모든 정보 새로고침
+        4. 매매 실행
+     */
+    @Scheduled(fixedDelay = 30000)
+    public synchronized void execute() throws IOException, NoSuchAlgorithmException {
+        
+        try {
+            orderListAPI.getOrders(); // 미체결 주문 확인
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            log.error("Error updating order list data", e);
+            throw new RuntimeException(e);
+        }
 
         cancelOrderAPI.cancelAll(); // 미체결 주문들을 모두 취소 (오류 방지)
 
-        apiService.updateAllData(); // 모든 API들의 정보를 업데이트
+        apiService.init(); // 모든 API들의 정보를 업데이트
         currentAsset.getAsset();
         orderService.sendOrder();   // 매매 조건에 맞으면 매매
     }
--- src/main/java/com/zestbear/bitcoin/mybitcoin/service/UpbitAPI/APIService.java
+++ src/main/java/com/zestbear/bitcoin/mybitcoin/service/UpbitAPI/APIService.java
@@ -4,19 +4,16 @@ import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Account.AccountAPI;
 import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Candle.CurrentValueAPI;
 import com.zestbear.bitcoin.mybitcoin.service.UpbitAPI.Candle.MinuteCandleAPI;
 import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Service
 public class APIService {
-
-    private static final Logger logger = LoggerFactory.getLogger(APIService.class);
 
     private final AccountAPI accountAPI;
     private final CurrentValueAPI currentValueAPI;
@@ -32,41 +29,47 @@ public class APIService {
     @PostConstruct
     public void init() {
         try {
-            updateAllData();
-        } catch (IOException | ExecutionException | InterruptedException e) {
+            updateAccount();
+            updateCurrentValue();
+            updateMinuteCandle();
+        } catch (RuntimeException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void updateAllData() throws IOException, ExecutionException, InterruptedException {
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
-            try {
-                accountAPI.getAccountsAPI();
-            } catch (ExecutionException | InterruptedException e) {
-                logger.error("Error updating account data", e);
-                throw new RuntimeException(e);
-            }
-        });
-        future1.get();
+    /*
+        내 계좌의 정보를 불러오는 API
+     */
+    public synchronized void updateAccount() {
+        try {
+            accountAPI.getAccountsAPI();
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("Error updating account data", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
-            try {
-                currentValueAPI.getCurrentValueAPI();
-            } catch (IOException | ExecutionException | InterruptedException e) {
-                logger.error("Error updating current value data", e);
-                throw new RuntimeException(e);
-            }
-        });
-        future2.get();
+    /*
+        현재 시세를 얻어오는 API
+     */
+    public synchronized void updateCurrentValue() {
+        try {
+            currentValueAPI.getCurrentValueAPI();
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            log.error("Error updating current value data", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-        CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
-            try {
-                minuteCandleAPI.getMinuteCandleAPI();
-            } catch (ExecutionException | InterruptedException e) {
-                logger.error("Error updating minute candle data", e);
-                throw new RuntimeException(e);
-            }
-        });
-        future3.get();
+    /*
+        이전 200개의 분봉을 얻어오는 API
+     */
+    public synchronized void updateMinuteCandle() {
+        try {
+            minuteCandleAPI.getMinuteCandleAPI();
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("Error updating minute candle data", e);
+            throw new RuntimeException(e);
+        }
     }
 }
